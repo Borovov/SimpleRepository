13ddaf8d9cd190c0e0de7c92aabea8322dcfaea4
adminadmin <adminadmin@Ehidna.localdomain>
2019-07-17 09:25:05 +0300

    Finish commit

--- Algoritm.java
+++ Algoritm.java
@@ -8,7 +8,17 @@ public class Algoritm {
 		int mass[][] = new int[10][10];
 				
 		for (int i = 0; i < mass.length; i++) {
-			
+			int max = 0;
+			for (int j = 0; j < mass[i].length; j++) {
+				mass[i][j] = rand.nextInt(10);
+				System.out.print(mass[i][j] + " ");
+				
+				if (mass[i][j] > max) {
+					max = mass[i][j];
+				}				
+			}
+			System.out.print("  (" + max + ")");
+			System.out.println();
 		}
 		
 	}
