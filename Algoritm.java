207dc5c1b788060d3fda0d82b5f0fc26571e89f6
adminadmin <adminadmin@Ehidna.localdomain>
2019-07-17 09:24:23 +0300

    Second commit

--- Algoritm.java
+++ Algoritm.java
@@ -4,7 +4,12 @@ public class Algoritm {
 	
 	public static void main(String[] args) {
 		
-		
+		Random rand = new Random(100);
+		int mass[][] = new int[10][10];
+				
+		for (int i = 0; i < mass.length; i++) {
+			
+		}
 		
 	}
 }
